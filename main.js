63fac413eff117b0983ab0f6d67d8d3bc500b15a
Hubert Madej <hubert.madej@gmail.com>
2021-01-29 01:42:12 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const firstName='Hubert';
+const age=33;
+
+console.log(firstName);
+console.log(age)
+console.log(`Nazywam się ${firstName} i mam ${age} lata.`);
+
+const myname='Dr Panda';
+const time=13;
+
+console.log(myname);
+console.log(time);
+console.log(`Hello my friend! My name is ${myname}. I am programer about ${time} years.`)
